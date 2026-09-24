ebc8c441ee39812011caf5b04fefa98401e2af56
Emi Celis <cdmx3107@amerike.edu.mx>
2026-09-24 16:11:39 -0600

    Subiendo cambios con bullet manager

--- Assets/Scripts/SimpleGunShoot.cs
+++ Assets/Scripts/SimpleGunShoot.cs
@@ -6,21 +6,15 @@ public class SimpleGunShoot : MonoBehaviour
     public Transform firePoint;
     public float bulletSpeed = 30f;
 
+
     public void Shoot()
     {
-     
         GameObject bullet = BulletManager.Instance.GetBullet();
 
-        
         bullet.transform.position = firePoint.position;
         bullet.transform.rotation = firePoint.rotation;
 
-        
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
-
-        
         rb.linearVelocity = firePoint.forward * bulletSpeed;
-
-       
     }
 }
